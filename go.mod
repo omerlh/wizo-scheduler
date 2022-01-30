072f3408203eba27e81dbce9caf2d1194dea4385
Omer Levi Hevroni <omer.levihevroni@snyk.io>
2022-01-30 20:28:34 +0200

    support group by option + ux fixes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/omerlh/wizo-schedule
 
 go 1.16
 
-require github.com/ahmetb/go-linq/v3 v3.2.0 // indirect
+require (
+	github.com/ahmetb/go-linq/v3 v3.2.0 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
     "encoding/csv"
     "fmt"
     "io"
-    "log"
     "os"
+		"github.com/jessevdk/go-flags"
 )
 
 type Row struct {
@@ -18,71 +18,111 @@ type Row struct {
 	Group string
 }
 
+const (
+	groupColumn = 4
+	hourColumn = 3
+	teacherColumn = 1
+	dayColumn = 2
+	locationColumn = 5
+	kidsColumn = 6
+)
+
+var (
+	fileName string
+	groupBy string
+)
+
+type Options struct {
+	FileName string `short:"f" long:"file" description:"The name of the CSV file to parse" required:"true"`
+	GroupBy string `short:"g" long:"groupBy" description:"The field to group by (teacher or group)" choice:"teacher" choice:"group" default:"teacher"`
+}
+
+
 func main() {
-    argsWithoutProg := os.Args[1:]
+	var options Options
 
-    f, err := os.Open(argsWithoutProg[0])
+	parser := flags.NewParser(&options, flags.Default)
 
-    if err != nil {
-
-        log.Fatal(err)
-    }
-
-    r := csv.NewReader(f)
-
-		_, err = r.Read()
-
-		if err != nil {
-			log.Fatal(err)
+	if _, err := parser.Parse(); err != nil {
+		switch flagsErr := err.(type) {
+		case flags.ErrorType:
+			if flagsErr == flags.ErrHelp {
+				os.Exit(0)
+			}
+			os.Exit(1)
+		default:
+			os.Exit(1)
 		}
+	}
 
-		rows := make([]Row, 1)
+	f, err := os.Open(options.FileName)
 
-    for {
+	if err != nil {
+		panic(err)
+	}
 
-        record, err := r.Read()
+	r := csv.NewReader(f)
 
-        if err == io.EOF {
-            break
-        }
+	// Skip headers
+	_, err = r.Read()
 
-        if err != nil {
-            log.Fatal(err)
-        }
+	if err != nil {
+		panic(err)
+	}
 
-        row := Row{
-					Kids: record[9],
-					Location: record[8],
-					Day: record[2],
-					Teacher: record[1],
-					Hour: record[3],
-					Group: record[4],
-				}
+	rows := make([]Row, 1)
 
-				rows = append(rows, row)
-    }
+	for {
 
-		var grouped []interface{}
+			record, err := r.Read()
 
-		From(rows).GroupBy(func(row interface{}) interface{} {
-			return row.(Row).Teacher // author as key
-		}, func(row interface{}) interface{} {
-			return row // author as value
-		}).ToSlice(&grouped)
+			if err == io.EOF {
+					break
+			}
 
-		for _, v := range(grouped) {
-			
-			fileName := fmt.Sprintf("%s.html", v.(Group).Key)
-
-			f, err := os.Create(fileName)
 			if err != nil {
 				panic(err)
 			}
 
-			defer f.Close()
+			row := Row{
+				Kids: record[kidsColumn],
+				Location: record[locationColumn],
+				Day: record[dayColumn],
+				Teacher: record[teacherColumn],
+				Hour: record[hourColumn],
+				Group: record[groupColumn],
+			}
 
-			f.WriteString(fmt.Sprintf(`<html>
-			<head>
+			rows = append(rows, row)
+	}
+
+	var grouped []interface{}
+
+	From(rows).GroupBy(func(row interface{}) interface{} {
+		if (options.GroupBy == "teacher") {
+			return row.(Row).Teacher 
+		} else {
+			return row.(Row).Group
+		}
+		// author as key
+	}, func(row interface{}) interface{} {
+		return row // author as value
+	}).ToSlice(&grouped)
+
+	for _, v := range(grouped) {
+		
+		fileName := fmt.Sprintf("%s.html", v.(Group).Key)
+
+		f, err := os.Create(fileName)
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+
+		f.WriteString(fmt.Sprintf(`<html dir="rtl" lang="he">
+		<head>
+		<meta charset="utf-8">
 <style>
 table {
   font-family: arial, sans-serif;
@@ -96,41 +136,43 @@ td, th {
   padding: 8px;
 }
 
-tr:nth-child(even) {
-  background-color: #dddddd;
+td.bold {
+	font-weight: bold;
+	font-size: larger;
 }
+
 </style>
 </head><body>`))
 
-			f.WriteString(fmt.Sprintf("<h1>%s</h1>", v.(Group).Key))
+		f.WriteString(fmt.Sprintf("<h1>%s</h1>", v.(Group).Key))
 
-			var byDay []interface{}
+		var byDay []interface{}
 
-			From(v.(Group).Group).GroupBy(func(row interface{}) interface{} {
-				return row.(Row).Day // author as key
-			}, func(row interface{}) interface{} {
-				return row // author as value
-			}).ToSlice(&byDay)
+		From(v.(Group).Group).GroupBy(func(row interface{}) interface{} {
+			return row.(Row).Day // author as key
+		}, func(row interface{}) interface{} {
+			return row // author as value
+		}).ToSlice(&byDay)
 
-			f.WriteString(fmt.Sprintf("<table><tr><th>זמן</th><th>מיקום</th><th>קבוצה</th></tr>"))
-			
-			for _, day := range(byDay) {
-				f.WriteString(fmt.Sprintf("<tr><td>%s</td><td></td><td></td></tr>\n", day.(Group).Key))
+		f.WriteString(fmt.Sprintf("<table><tr><th>זמן</th><th>מיקום</th><th>קבוצה</th></tr>"))
+		
+		for _, day := range(byDay) {
+			f.WriteString(fmt.Sprintf("<tr><td class=bold>%s</td><td></td><td></td></tr>\n", day.(Group).Key))
 
-				var sorted []interface{}
+			var sorted []interface{}
 
-				From(day.(Group).Group).OrderByDescending( // sort groups by its length
-					func(group interface{}) interface{} {
-						return len(group.(Row).Hour)
-					}).ToSlice(&sorted)
+			From(day.(Group).Group).OrderByDescending( // sort groups by its length
+				func(group interface{}) interface{} {
+					return len(group.(Row).Hour)
+				}).ToSlice(&sorted)
 
-				for _, hour := range(sorted) {
-					f.WriteString(fmt.Sprintf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n", hour.(Row).Hour, hour.(Row).Location, hour.(Row).Group))
-				}
+			for _, hour := range(sorted) {
+				f.WriteString(fmt.Sprintf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n", hour.(Row).Hour, hour.(Row).Location, hour.(Row).Group))
 			}
-
-			f.WriteString(fmt.Sprintf("</table></html></body>"))
 		}
+
+		f.WriteString(fmt.Sprintf("</table></html></body>"))
+	}
 
 		
 }
